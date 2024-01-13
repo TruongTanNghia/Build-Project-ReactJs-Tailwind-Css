1119336cccda34ec6b81b7343ceced7341327059
TruongTanNghia <tannghiaxp@gmail.com>
2024-01-13 22:16:27 +0700

    feat: #3 [Section] - Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+//* Lib
 import React from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+//* Import Components
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Services from "./components/Services/Services.jsx";
@@ -7,8 +11,6 @@ import AppStore from "./components/AppStore/AppStore.jsx";
 import CoverBanner from "./components/CoverBanner/CoverBanner.jsx";
 import Testimonial from "./components/Testimonial/Testimonial.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 const App = () => {
   React.useEffect(() => {
@@ -23,8 +25,8 @@ const App = () => {
 
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
-      <Navbar />
-      <Hero />
+      <Navbar/>
+      <Hero/>
       <Services />
       <Banner />
       {/* <CoverBanner /> */}
