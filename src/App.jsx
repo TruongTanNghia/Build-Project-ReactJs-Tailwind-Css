e0ee502710078975f4700a8b8bf6737c76c6efc6
TruongTanNghia <tannghiaxp@gmail.com>
2024-01-14 23:00:50 +0700

    feat: #6 [Section] - App Store section

--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,10 @@ const App = () => {
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
       <Navbar/>
       <Hero/>
-      <Services />
-      <Banner />
+      <Services/>
+      <Banner/>
       {/* <CoverBanner /> */}
-      <AppStore />
+      <AppStore/>
       <Testimonial />
       <Footer />
     </div>
